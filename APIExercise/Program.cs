4e55bbbde4ab168067ec0ceb2beb2a7e2b4169e4
danisylvester <maria.sylvester10@gmail.com>
2020-09-23 11:05:09 -0500

    Created QuoteGenerator class that receives quotes from a Ron Swanson and Kanye West api. Added loop in program to simulate a conversation between Kanye & Ron.

---
+++ APIExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var qGenerator = new QuoteGenerator(client);
+
+
+            for(int i = 0; i < 5; i++)
+            {
+                var kanyeQuote = qGenerator.GetKanyeQuote();
+                Console.WriteLine($"Kanye: '{kanyeQuote}'");
+                Console.WriteLine();
+
+                var ronQuote = qGenerator.GetRonSwansonQuote();
+                Console.WriteLine($"Ron Swanson: '{ronQuote}'");
+                Console.WriteLine("....................................................");
+            }
+        }
+    }
+}
---
+++ APIExercise/QuoteGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIExercise
+{
+    public class QuoteGenerator
+    {
+        public QuoteGenerator(HttpClient client)
+        {
+            _client = client;
+        }
+        
+        public HttpClient _client = new HttpClient();
+        string kanyeURL = "https://api.kanye.rest";
+        string ronSwansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+
+        public string GetKanyeQuote()
+        {
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+            return JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+        }
+
+        public string GetRonSwansonQuote()
+        {
+            var ronResponse = _client.GetStringAsync(ronSwansonURL).Result;
+            return JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+        }
+    }
+}
